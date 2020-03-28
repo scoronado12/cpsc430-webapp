5aa2eb5751b21260996a2999499bdd67d042af4d
Stefano C. Coronado <stefanocorona12@gmail.com>
2020-03-27 20:21:43 -0400

    stock index page

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,30 +1,11 @@
-//import Layout from '../components/Layout'
 
-class Page extends React.Component {
-
-  render() {
-    return (
-      <Layout>
-  //      <div class="container">
-    //      <div class="starter text-center">
-            <h1>Bootstrap starter template</h1>
-            <p class="lead">Use this document as a way to quickly start any new project.<br/> All you get is this text and a mostly barebones HTML document.</p>
-//           </div>
-//         </div>
-      </Layout>
-    )
-  }
-}
-export default Page
-
-
-/*function HomePage() {
-  var 
+function HomePage() {
+   
   return <div>Welcome to Next.js!</div>
 }
 
 export default HomePage
 
-*/
+
 
 //app.listen(3000)
