532f2ea860bec1fb03845e5e511d107301c6b42e
Bistik <dmulyono@umw.mail.edu>
2020-04-07 12:54:36 -0400

    Need to test if this code works but not familiar with js...

--- webapp/components/AlumniQuery.js
+++ webapp/components/AlumniQuery.js
@@ -12,30 +12,50 @@ var pool = mysql.createPool({
     debug: false
 });
 
-// Shows the alumnis list.
-function showAlumnis(req, res) {
-    pool.query("SELECT * FROM alumnis", (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        } else {
-            res.json(result);
-        }
-    });
-}
+app.get("/", async (req, res) => {
+    var field = (req.query !== "") ? "%" + req.query + "%" : "";
+    var input;
 
-// Search for alumni by graduation year.
-function findGradYear(year) {
-    var query = mysql.format("SELECT * FROM alumnis WHERE graduation_year = ?", [year]);
-    var result = pool.query(query);
-    res.json(result.rows);
-}
+    switch (field) {
+        case "email":
+            input = (req.query.email !== "") ? "%" + req.query.email + "%" : "";
+            break;
+        case "first_name":
+            input = (req.query.first_name !== "") ? "%" + req.query.first_name + "%" : "";
+            break;
+        case "last_name":
+            input = (req.query.last_name !== "") ? "%" + req.query.last_name + "%" : "";
+            break;
+        case "major":
+            input = (req.query.major !== "") ? "%" + req.query.major + "%" : "";
+            break;
+        case "graduation_year":
+            input = (req.query.graduation_year !== "") ? "%" + req.query.graduation_year + "%" : "";
+            break;
+        case "occupation":
+            input = (req.query.occupation !== "") ? "%" + req.query.occupation + "%" : "";
+            break;
+        case "newletter_opt_in":
+            input = (req.query.newletter_opt_in !== "") ? "%" + req.query.newletter_opt_in + "%" : "";
+            break;
+        default:
+            return; // Breaking out, invalid inputs.
+    }
 
-// Search for alumni by occupation.
-function findOccupation(req, res) {
-    var query = mysql.format("SELECT * FROM alumnis WHERE occupation = ?", [req]);
-    var result = pool.query(query);
-    res.json(result.rows);
-}
+    try {
+        var result = await pool.query("SELECT * FROM alumnis WHERE $1 = $2", [field, input]);
 
-app.get("
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+        res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,contenttype");
+        res.setHeader("Access-Control-Allow-Credentials", true);
+
+        res.json(result.rows);
+    } catch (error) {
+        console.log("Error running request: ", error);
+    }
+});
+
+app.listen(app.get("port"), () => {
+    // Running and listening for connection.
+});
