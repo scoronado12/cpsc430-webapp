4dab7eea8ff061dc3a9b63a2be110766ec6725c1
Stefano C. Coronado <stefanocorona12@gmail.com>
2020-04-09 12:56:59 -0400

    Added sendgrid test key, email page will now send email to me

--- webapp/pages/api/sendgrid-server.js
+++ webapp/pages/api/sendgrid-server.js
@@ -2,8 +2,9 @@ const sgMail = require('@sendgrid/mail')
 
 
 export default async function(req, res) {
+    
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
+    /*TODO populate a bunch of different addresses in-lieu of the the hard-coded email address*/
     const { subject, message } = req.body
     console.log(req.body);
     const content = {
@@ -13,12 +14,11 @@ export default async function(req, res) {
         text: `${message}`,
         html: `<p>${message}</p>`
     }
-    /*
     try {
         await sgMail.send(content)
         res.status(200).send('Message sent successfully.')
     } catch (error) {
         console.log('ERROR', error)
         res.status(400).send('Message not sent.')
-    }*/
+    }
 }
