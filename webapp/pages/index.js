35cfe4d9ed7b13c61cdee7342cf2ef4606b07305
Daniel <danielmulyono@gmail.com>
2020-04-15 12:29:19 -0400

    GET request working, wrote skeleton code for Alumni search page (only the API calls).

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -27,7 +27,7 @@ export default function Index() {
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
             crossOrigin="anonymous"/>
-            {testCall()}
+            {testCall()} // Testing server.
         </div>
     
         
