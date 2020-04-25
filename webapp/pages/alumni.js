8837d147f7e60c3cfa82d348cabc03eaccd1ad32
Chance-F <chance@coyotepack.com>
2020-04-25 16:27:07 -0400

    changed alumni to match email

--- webapp/pages/alumni.js
+++ webapp/pages/alumni.js
@@ -1,215 +1,283 @@
-import axios from 'axios';
-import Layout from '../components/MyLayout'
+import React, {useState} from "react"
 import Link from 'next/link'
+import Layout from '../components/MyLayout'
 import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap'
 
-class Alumni extends React.Component {
-    
-    constructor(props){
+
+
+export default () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null }
+  })
+
+  const [inputs, setInputs] = useState({
+    subject: '',
+    message: ''
+  })
+
+  const handleResponse = (status, msg) => {
+    if (status === 200) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg }
+      })
+      setInputs({
+        subject: '',
+        message: ''
+      })
+    } else {
+      setStatus({
+        info: { error: true, msg: msg }
+      })
+    }
+  }
+
+  const handleOnChange = e => {
+    e.persist()
+    setInputs(prev => ({
+      ...prev,
+      [e.target.id]: e.target.value
+    }))
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null }
+    })
+  }
+
+  const handleOnSubmit = async e => {
+    e.preventDefault()
+    setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
+    const res = await fetch('/api/sendgrid-server', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputs)
+    })
+    const text = await res.text()
+    handleResponse(res.status, text)
+  }
+
+
+
+      return (
+     <Layout>
+      <main>
+        <div> 
+            <link rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossOrigin="anonymous"/> 
+        </div>
+
         
-        super(props);
-        this.state = {
-            fname: "",
-            lname: "",
-            email_addr: "",
-            occupation: "",
-            degree: "",
-            grad_yr: "",
-            bio: "",
-            newsletter_optin: ""
-        };
-    }
-    
-    handleUpdatefname(evt){
-        this.setState({fname: evt.target.value});
-    }
-    
-    
-    handleUpdatelname(evt){
-        this.setState({lname: evt.target.value});
-    }
-    
-        
-    handleUpdateemail(evt){
-        this.setState({email_addr: evt.target.value});
-    }
-           
-    handleUpdateOccupation(evt){
-        this.setState({occupation: evt.target.value});
-    }
-    handleUpdateGradyear(evt){
-        this.setState({grad_yr: evt.target.value});
-    }
-    
-    handleUpdateDegree(evt){
-        this.setState({degree: evt.target.value})
-    }
-    
-    handleUpdateBio(evt){
-        this.setState({bio: evt.target.value});
-    }
-    
-    handle_newsletter_optin(evt){
-        this.setState({newsletter_optin: evt.target.value});
-    }
+        <Navbar bg="medium" expand="lg">
+          <Navbar.Brand href="#home">EESAD</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>  
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+        <div id="earthIMG"><img src="/earth.png" style={{maxWidth:"150px", maxHeight:"150px"}}/></div>
+        <div id="UMWIMG"><img src="/umwEagle.png" style={{maxWidth:"100px", maxHeight:"100px"}}/></div>
+        <h1 className="title">UMW</h1>
+        <h1 className="titletwo"> Earth Science Alumni Database </h1>
+        <div className="floating-box">
+            <h2>Send an email to all registered users</h2>
 
-    async handle_submission(evt) {
-        evt.preventDefault();
-        
-        console.log("Name: " + fname);
-        console.log("Lname: " + lname);
-        console.log("email: " + email);
-        console.log("grad_yr: " + grad_yr);
-        console.log("bio: " + bio);
-        console.log("newsletter? " + newsleter_optin); 
-        /*
-        axios.post('http://127.0.0.1:8000/alumni_insert', {
-            
-            first_name: "Tester",
-            last_name: "McTesterson",
-            email: "hardcode@stefanocoronado.com",
-            occupation: "student",
-            degree_obtained: "Computer Science",
-            grad_year: "2020",
-            bio: "Hello world from the hardcoded api call",
-            newsletter_optin: "1",
-            
-        }).then((response) => {
-            console.log(response.data);
-        }); /*unhandled response rejection warning error may occur*/ 
-    }
+        <form onSubmit={handleOnSubmit}>
+            <label htmlFor="fname">Frist Name</label>
+            <input
+              id="fname"
+              type="text"
+              onChange={handleOnChange}
+              required
+              value={inputs.fname}
+             />
 
+            <label htmlFor="lname">Last Name</label>
+            <input
+              id="lname"
+              onChange={handleOnChange}
+              required
+              value={inputs.lname}
+            />
+            <label htmlFor="Occupation">Occupation</label>
+            <input
+              id="occupation"
+              onChange={handleOnChange}
+              required
+              value={inputs.occupation}
+            />
+            <label htmlFor="gradyr">Graduation Year</label>
+            <input
+              id="gradyr"
+              onChange={handleOnChange}
+              required
+              value={inputs.grad_yr}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              onChange={handleOnChange}
+              required
+              value={inputs.email}
+            />
+            <label htmlFor="message">Message</label>
+            <input
+              id="message"
+              onChange={handleOnChange}
+              required
+              value={inputs.message}
+            />
+            <label htmlFor="degree">Degree Obtained</label>
+            <input
+              id="degree"
+              onChange={handleOnChange}
+              required
+              value={inputs.degree}
+            />
+            <label htmlFor="bio">Bio</label>
+            <textarea
+              id="bio"
+              onChange={handleOnChange}
+              required
+              value={inputs.bio}
+            />
+            <button type="submit" disabled={status.submitting}>
+              {!status.submitting
+                ? !status.submitted
+                  ? 'Submit'
+                  : 'Submitted'
+                : 'Submitting...'}
+            </button>
+        </form>
+          {status.info.error && (
+            <div className="error">Error: {status.info.msg}</div>
+          )}
+          {!status.info.error && status.info.msg && (
+            <div className="success">{status.info.msg}</div>
+          )}
 
-
-
-
-    render() {
-    return (
-        <Layout>
-        <main>
-            <div> 
-                <link rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-                integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-                crossorigin="anonymous"/> 
-                
-            </div>
-                <Navbar bg="medium" expand="lg">
-                <Navbar.Brand href="#home">EESAD</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>  
-            </Nav>
-            </Navbar.Collapse>
-            </Navbar>
-            <div id="earthIMG"><img src="/earth.png" style={{maxWidth:"150px", maxHeight:"150px"}}/></div>
-            <div id="UMWIMG"><img src="/umwEagle.png" style={{maxWidth:"100px", maxHeight:"100px"}}/></div>
-            <h1 className="title"> Enter Your Information Below! </h1>
-            <div className="boxed" bg="medium">
-                <form onSubmit={this.handle_submission}>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="first_name_line">
-                        <Form.Label>First Name</Form.Label>
-                        <input type='text' value={this.state.fname} onChange={this.handleUpdatefname.bind(this)}/>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="last_name_line">
-                        <Form.Label>Last Name</Form.Label>
-                        <input type='text' value={this.state.lname} onChange={this.handleUpdatelname.bind(this)}/>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="email_line">
-                        <Form.Label>Email</Form.Label>
-                        <input type='text' value={this.state.email} onChange={this.handleUpdateemail.bind(this)}/>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="occupation_line">
-                        <Form.Label>Occupation</Form.Label>
-                        <input type='text' value={this.state.occupation} onChange={this.handleUpdateOccupation.bind(this)}/>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="degree_line">
-                        <Form.Label>Degree Obtained</Form.Label>
-                        <input type='text' value={this.state.degree} onChange={this.handleUpdateDegree.bind(this)}/>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="year_line">
-                        <Form.Label>Graduation Year</Form.Label>
-                        <input type='text' value={this.state.grad_yr} onChange={this.handleUpdateGradyear.bind(this)}/>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Group controlId="bio_line">
-                        <Form.Label>Bio </Form.Label>
-                        <input type='text' value={this.state.bio} onChange={this.handleUpdateBio.bind(this)}/>
-                    </Form.Group>
-                    <Form.Group id="checkbox">
-                        <Form.Check className="czekbox" type="checkbox" label="Opt into department Newsletter" value={this.state.newsleter_optin} onChange={this.handle_newsletter_optin.bind(this)}/>
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
-                        Submit </Button>
-                    </form>
-            </div> 
+        </div>
         </main>
-        <style type="text/css" jsx> {`
+        
 
-            .title {
-            text-align: center;
-            color: #03254c;
-            font-size: 62px;
-            font-weight: bold;
-            }
-            main {
-            position: fixed;
-            width: 100%;
-            height: 100%;
-            background-size: cover;
-            background-image: url("jepson.jpg");
-            font-family: "Times New Roman";
-            overflow: auto;
-            }
-            #earthIMG {
-            position: absolute;
-            top: 10px;
-            right: 100px;
-            }
-            #UMWIMG {
-            position: absolute;
-            top: 10px;
-            right 12.5px;
-            }
-            Navbar {
-            background-color: grey;
-            }
-            .boxed {
-            border: 2px solid #03254c;
-            background-color: rgba(211, 211, 211, 0.5);
-            text-align: center;
-            font-size: 32px;
-            margin-left: 12px;
-            margin-right: 12px;
-            padding-top: 15px;
-            padding-right: 15px;
-            padding-left: 15px;
-            padding-bottom: 15px;
-            } 
-            .form-control {
-                width: 50%;
-            }
-            #formGridCheckbox {
-                height: 50px;
-                width: 50px;
-            }
-        `}
-        </style>
-        </Layout>
-    );
-    }
+
+
+
+      <style type="text/css" jsx> {`
+
+         .title {
+          text-align: center;
+          color: #03254c;
+          font-size: 40px;
+          font-weight: bold;
+        }
+        .titletwo {
+          text-align: center;
+          font-size: 40px;
+          color: #03254c;
+          font-weight: bold;
+        }
+        main {
+          position: fixed;
+          width: 100%;
+          height: 100%;
+          background-size: cover;
+          background-image: url("jepson.jpg");
+          font-family: "Times New Roman";
+          overflow: auto;
+
+        }
+        .button {
+          background-color: #03254c;
+          color: white;
+          padding: 14px 40px;
+          border-radius: 8px;
+        }
+        #earthIMG {
+          position: absolute;
+          top: 10px;
+          right: 100px;
+        }
+        #UMWIMG {
+          position: absolute;
+          top: 10px;
+          right 12.5px;
+        }
+        Navbar {
+          background-color: grey;
+          color: #03254c;
+          font-weight: bold;
+        }
+        .floating-box {
+         position: centered;
+         margin-left: 12px;
+          margin-right: 12px;
+          padding-top: 15px;
+          padding-right:15px;
+          padding-left: 15px;
+          padding-bottom: 15px;
+          border: 5px solid #03254c; 
+          background-color: #CDCDCD; 
+          border-radius: 8px;
+
+        }
+        .buttons {
+         
+        }
+        .enterButton {
+          opacity: 10;
+          text-align: center;
+          background-color: #03254c;
+          color: white;
+          width: 15%;
+          position: fixed;
+          padding: 14px 40px;
+          border-radius: 8px;
+          font-size: 24px;
+          justify-content: center;
+          top: 38%;
+          left: 42.5%;
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+          flex: 1 1 0%;
+          transition-duration: 0.4s;
+        }
+        .adminButton {
+          width: 15%;
+          opacity: 10;
+          text-align: center;
+          background-color: #03254c;
+          color: white;
+          position: fixed;
+          padding: 14px 40px;
+          border-radius: 8px;
+          font-size: 24px;
+          top: 50%;
+          left: 42.5%;
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+          transition-duration: 0.4s;
+          flex: 1 1 0%;
+        }
+        .adminButton:hover {
+          background-color: #B53737;
+          color: white;
+        }
+        .enterButton:hover {
+          background-color: #B53737;
+          color: white;
+        }
+    `}
+      </style>
+    </Layout>
+      )
 }
-
-export default Alumni;
